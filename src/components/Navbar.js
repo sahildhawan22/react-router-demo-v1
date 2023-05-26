3cada0806466ed3950a5f54ee0acdf600ebf60a2
Sahil Dhawan <sahildhawan22@gmail.com>
2023-05-26 13:07:38 +0530

    Link active style NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-export const Navbar = () => {
+/* export const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+        </nav>
+    )
+} */
+
+export const Navbar = () => {
+    const navLinkStyles = ({isActive}) => {
+        return {
+            fontWeight: isActive ? "bold" : "none",
+            textDecoration: isActive ? "none" : "underline"
+        }
+    }
+
+    return (
+        <nav>
+            <NavLink style={navLinkStyles} to="/">Home</NavLink>
+            <NavLink style={navLinkStyles} to="/about">About</NavLink>
         </nav>
     )
 }
