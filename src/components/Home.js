fbe72cc6673699017b490bef391318a04bd539fd
Sahil Dhawan <sahildhawan22@gmail.com>
2023-05-27 17:42:55 +0530

    Navigating programmatically

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,11 @@
+import { useNavigate } from "react-router-dom"
 export const Home = () => {
-    return <div>Home Page</div>
+    const navigate = useNavigate();
+    return (  
+        <>
+            <div>Home Page</div>
+            {/* navigate("order-summary", {replace: true}) */}
+            <button onClick={() => navigate("order-summary")}>Place Order</button>
+        </>
+    )
 }
